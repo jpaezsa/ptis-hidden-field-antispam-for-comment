27eb0e8db3c373bbf2d646697f4987d42a1cf0d4
ptipti <ptipti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-09 20:22:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ptis-hidden-field-antispam-for-comment/trunk@273567 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ptishiddenfield.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Pti's Hidden Field Antispam for comment
+Plugin URI: http://www.ptipti.ru/hiden_field/
+Description: A simple plugin wich adds hidden field to comment form, to exclude auto-spam possibility through POST request
+Author: Pti_the_Leader
+Version: 1.0
+Author URI: http://www.ptipti.ru/
+*/
+
+session_start();
+
+add_action ('comment_form', 'insert_hidden_field');
+add_action ('comment_post', 'check_hidden_field');
+
+
+function insert_hidden_field ($id) {
+	global $user_ID;
+	if( $user_ID ) {
+		return $id;
+	} else {
+		$key =  md5 (rand ());
+		$_SESSION['hidden_session'] = $key;
+		echo '<input type="hidden" name="hidden_form" value="'.$key.'" />';
+	}
+}
+
+function check_hidden_field ($id) {
+	global $user_ID;
+	if ($user_ID) {
+		return $id;
+	} else {
+		if ($_SESSION['hidden_session'] != $_POST['hidden_form']) {
+			wp_set_comment_status ($id, 'spam');
+		}
+	}
+}
+?>
